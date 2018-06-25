10d11d4cfe5b75dfd961201dfda8beba1ff89f97
Nickleus Jimenez <nickleus.jimenez@gmail.com>
2018-06-26 01:20:28 +0800

    Translate google Link

---
+++ Wikain/Wikain/wwwroot/js/utility.js
@@ -0,0 +1,13 @@
+﻿function RemoveElement(containerSelctor, elem) {
+    $(elem).addClass("hidden");
+
+}
+function makeId(length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < length; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
